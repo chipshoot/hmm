d08273b58414e7b76c2dc5d2955defcb986c6510
Chaoyang Fang <fchy5979@gmail.com>
2017-07-31 17:36:02 -0400

    add change for code sync

---
+++ Hmm.Dal/DefaultRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Hmm.Utility.Dal;
+
+namespace Hmm.Dal
+{
+    public class DefaultRepository<T> : IRepository<T> where T : Entity
+    {
+        public IQueryable<T> FindEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindEntities(Expression<Func<T, bool>> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Refresh(ref T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindEntityById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Hmm.DalTests/NoteRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using DomainEntity.Misc;
+using Hmm.Dal;
+using Xunit;
+
+namespace Hmm.DalTests
+{
+    public class NoteRepositoryTests
+    {
+        [Fact]
+        public void CanAddNoteToRepository()
+        {
+            // Arrange
+            var repo = new DefaultRepository<HmmNote>();
+            var note = new HmmNote
+            {
+                Id = 1,
+                Author = 1,
+
+            };
+            // Act
+
+            // Assert
+
+        }
+    }
+}
