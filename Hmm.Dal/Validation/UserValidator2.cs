16299a395554ea60a882489c8cb0c38215c43484
Chaoyang Fang <fchy5979@gmail.com>
2018-07-16 12:01:45 -0400

    Try to use FluentValidator to all DAL objects

---
+++ Hmm.Dal/Validation/UserValidator2.cs
@@ -0,0 +1,18 @@
+﻿using DomainEntity.User;
+using FluentValidation;
+
+namespace Hmm.Dal.Validation
+{
+    public class UserValidator2 : AbstractValidator<User>
+    {
+        public UserValidator2()
+        {
+            RuleFor(u => u.FirstName).NotNull().Length(100);
+            RuleFor(u => u.LastName).NotNull().Length(100);
+            RuleFor(u => u.BirthDay).NotNull();
+            RuleFor(u => u.AccountName).NotNull().Length(256);
+            RuleFor(u => u.Password).NotNull().Length(128);
+            RuleFor(u => u.Description).Length(1000);
+        }
+    }
+}
